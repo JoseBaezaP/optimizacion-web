2ac24c187abac442b460ce755ac92c4590b66195
jose de la rosa <josedelarosabass@gmail.com>
2021-05-25 11:09:22 -0500

    add the images

--- rss/resources/views/news.blade.php
+++ rss/resources/views/news.blade.php
@@ -1,57 +1,56 @@
 @extends('layout.app')
-@section('content') 
-    <div class="container">
+@section('content')
+<div class="container">
     @include('partials.notification')
     <div class="row">
-    <div class="col-2">
-    </div>
-     <div class="col-8">
-      <div class="row">
-        <div class="col-6">
-          <h1 class="m-2"> <image src="\logo.jpeg" class="navbar-brand"> <a href="/news" class="text-decoration-none">Rss</a> </h1> 
+        <div class="col-2">
         </div>
-        <div class="col-6 d-flex align-refresh">
-          <form action="/refresh" method="post">
-            @csrf       
-            <button type="submit" class="m-2 btn link-primary">Actualizar</button>
-          </form>
-        </div>
-      </div>
-        <div class="row">
-          <div class="col-4 ">
-            @include('partials.inputSearch')
-          </div>
-          <div class="col-8 text-end">
-            <a href="/post-feed"><button type="button" class="btn btn-outline-primary">Agregar más noticias</button></a>  
-          </div>
-        </div>
-        @include('partials.dropdown')
-      <div>
-      @if(count($news)>0)
-        @foreach($news as $new)
-        <div class="card  mb-2">
-          <div class="card-header">
+        <div class="col-8">
             <div class="row">
-              <div class="col-8">
-                {{$new->title}}
-              </div>
-              <div class="col-4 text-end">
-                <span>{{$new->date}}</span>
-              </div>
+                <image src="\images\evolution.png" class="navbar-brand w-100">
+                    <div class="col-12 d-flex align-refresh">
+                        <form action="/refresh" method="post">
+                            @csrf
+                            <button type="submit" class="m-2 btn btn-primary">Actualizar</button>
+                        </form>
+                    </div>
             </div>
-          </div>
-          <div class="card-body">
-            <p class="card-text">  {{$new->description}}</p>
-            <a href="{{$new->link}}" class="btn btn-primary" target="__blank" rel="nofollow noopener noreferrer">Leer noticia</a>
-          </div>
+            <div class="row">
+                <div class="col-4 ">
+                    @include('partials.inputSearch')
+                </div>
+                <div class="col-8 text-end">
+                    <a href="/post-feed"><button type="button" class="btn btn-primary">Agregar más
+                            noticias</button></a>
+                </div>
+            </div>
+            @include('partials.dropdown')
+            <div>
+                @if(count($news)>0)
+                @foreach($news as $new)
+                <div class="card  mb-2">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-8">
+                                {{$new->title}}
+                            </div>
+                            <div class="col-4 text-end">
+                                <span>{{$new->date}}</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text"> {{$new->description}}</p>
+                        <a href="{{$new->link}}" class="btn btn-primary" target="__blank"
+                            rel="nofollow noopener noreferrer">Leer noticia</a>
+                    </div>
+                </div>
+                @endforeach
+                @else
+                <div class="p-3 mb-2 bg-light text-dark">No se encontraron noticias.</div>
+                @endif
+            </div>
         </div>
-        @endforeach
-      @else
-        <div class="p-3 mb-2 bg-light text-dark">No se encontraron noticias.</div>
-      @endif
-      </div>
-      </div>
-      <div class="col-2" style="background-image: url('\vertical.jpg'); background-repeat: repeat-y;">
     </div>
-    </div>
+</div>
 @endsection
--- rss/resources/views/partials/inputSearch.blade.php
+++ rss/resources/views/partials/inputSearch.blade.php
@@ -2,6 +2,6 @@
           @csrf
           <div class="input-group mb-3">
             <input type="text" class="form-control" placeholder="Buscar" name="search" required>
-            <button class="btn btn-outline-secondary" type="submit">Buscar</button>
+            <button class="btn btn-secondary" type="submit">Buscar</button>
           </div>
         </form>
